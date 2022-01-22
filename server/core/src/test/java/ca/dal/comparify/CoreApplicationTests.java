2a3a22d1f6f417bacb33c2308990c73e0926ee40
handsomecoder <harshshah1295@gmail.com>
2022-01-22 11:57:02 -0400

    Created microservices of core and analytics

---
+++ server/core/src/test/java/ca/dal/comparify/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.dal.comparify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
