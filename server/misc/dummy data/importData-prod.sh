86ca20f1b38225e94466ae46a5b8695fa6bf3686
Chanpreet Singh <ch835893@dal.ca>
2022-03-15 15:20:10 -0300

    Dummy data and import files

---
+++ server/misc/dummy data/importData-prod.sh
@@ -0,0 +1,3 @@
+mongoimport --db comparify --collection Category --file Category.json --jsonArray
+mongoimport --db comparify --collection securityQuestion --file securityQuestion.json --jsonArray
+mongoimport --db comparify --collection Store --file Store.json --jsonArray
