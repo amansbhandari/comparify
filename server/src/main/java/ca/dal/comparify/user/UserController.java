0dfed681ec9579771c3792226739b5cec57437c2
shah3 <hr653326@dal.ca>
2022-03-26 18:21:45 -0300

    Build fix

--- server/src/main/java/ca/dal/comparify/user/UserController.java
+++ server/src/main/java/ca/dal/comparify/user/UserController.java
@@ -9,10 +9,7 @@ import ca.dal.comparify.user.service.UserService;
 import ca.dal.comparify.utils.ResponseEntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
