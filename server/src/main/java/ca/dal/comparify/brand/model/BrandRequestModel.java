ff0bcd87f2bb3ded91c4190f44c2a9a35f84d3c0
Chanpreet Singh <ch835893@dal.ca>
2022-03-27 16:58:49 -0300

    Added default constructor in BrandRequestModel

--- server/src/main/java/ca/dal/comparify/brand/model/BrandRequestModel.java
+++ server/src/main/java/ca/dal/comparify/brand/model/BrandRequestModel.java
@@ -9,6 +9,8 @@ public class BrandRequestModel {
 
     private String description;
 
+    public BrandRequestModel(){}
+
     public BrandRequestModel(String name, String description) {
         this.name = name;
         this.description = description;
