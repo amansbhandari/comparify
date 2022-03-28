44c34ffe41628cab298744bc414dbaf3bad5f4e4
Aman Bhandari <am727005@dal.ca>
2022-03-28 02:37:50 -0300

    Feature user profile

--- client/src/store/thunk/userThunkCreators.js
+++ client/src/store/thunk/userThunkCreators.js
@@ -6,8 +6,27 @@ export const authenication = (credentials) => async (dispatch) => {
       const { data } = await httpClient.post("/user/authentication", credentials);
       dispatch(gotAuth(data));
       localStorage.setItem("auth-token", data.token);
+      localStorage.setItem("user_identifier", credentials.user_identifier)
     } catch (error) {
       dispatch(failedAuth(error));
       localStorage.removeItem("auth-token")
     }
+};
+
+export const getDetails = (username) => {
+    return httpClient.get("/user/details?username=" + username).then((response) => {
+        let data = response.data
+        return data;
+    }, (error) => {
+      alert(error);
+    });
+};
+
+export const saveDetails = (details) => {
+  return httpClient.post("/user/details", details).then((response) => {
+        return details
+    }, (error) => {
+      alert(error);
+    });
+ 
 };
