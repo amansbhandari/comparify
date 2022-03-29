6e4b93407b8b3a34cef1fa15d3318906727b1bae
Qiwei Sun <qw351466@dal.ca>
2022-03-29 04:52:26 -0300

    remove unused

--- server/src/main/java/ca/dal/comparify/securityQuestion/repository/QuestionRepository.java
+++ server/src/main/java/ca/dal/comparify/securityQuestion/repository/QuestionRepository.java
@@ -1,6 +1,5 @@
 package ca.dal.comparify.securityQuestion.repository;
 
-import org.bson.BsonDocument;
 import org.bson.conversions.Bson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
