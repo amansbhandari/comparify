6b086c9ba0f6d1454557b5318b101782d47d55ac
Aman Bhandari <am727005@dal.ca>
2022-04-02 22:17:59 -0300

    Feature admin menus

---
+++ client/src/components/analytics/MediaCard.js
@@ -0,0 +1,26 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+export default function MediaCard(props) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="150"
+        image={props.image}
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
