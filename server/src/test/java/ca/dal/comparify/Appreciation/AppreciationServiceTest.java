bf90479b625644b2b7370a325b1c63e22d6d38c1
Chanpreet Singh <ch835893@dal.ca>
2022-04-05 02:17:51 -0300

    Test

--- server/src/test/java/ca/dal/comparify/Appreciation/AppreciationServiceTest.java
+++ server/src/test/java/ca/dal/comparify/Appreciation/AppreciationServiceTest.java
@@ -70,4 +70,43 @@ public class AppreciationServiceTest {
         assertEquals(appreciationService.addAppreciation(model),true);
     }
 
+    /**
+     * @author Chanpreet Singh
+     */
+    @Test
+    void addAppreciationTest2()
+    {
+        Date date = new Date();
+
+        CompareItemsModel model = new CompareItemsModel();
+        model.setStatus("verified");
+        model.setBrandId("6247c13f28da7f42a60913e2");
+        model.setDateOfPurchase(date);
+        model.setImageText("<base64>");
+        model.setPrice(20.0);
+        model.setPriceAfterDiscount(2.0);
+        model.setProductId("6247c13f28da7f42a60913e2");
+        model.setStoreId("6247c13f28da7f42a60913e2");
+        model.setUnit(2.0);
+        model.setId(new ObjectId(("6247c13f28da7f42a60913e3")));
+        List models = new ArrayList<>();
+        models.add(model);
+
+
+        UserDetailsModel userModel = new UserDetailsModel();
+        userModel.setId("6247c13f28da7f42a60913e3");
+        userModel.setUsername("john");
+        userModel.setType("silver");
+        userModel.setEmail("john@gmail.com");
+        userModel.setFirstName("John");
+        userModel.setLastName("Parker");
+        userModel.setPoints(49.0);
+
+        when(appreciateRepository.billItemsOfSameProducts(any())).thenReturn(models);
+        when(mongoRepository.findOne((String) any(), (Bson) any(), (Bson) any(),any())).thenReturn(userModel);
+        when(appreciateRepository.saveUserAppreciation(any())).thenReturn(false);
+
+        assertEquals(appreciationService.addAppreciation(model),false);
+    }
+
 }
